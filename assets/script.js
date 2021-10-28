1be3da4b9689addf51635cba72da82f5d17b19e0
Gurtej736l <gurtej154@gmail.com>
2021-10-28 11:29:50 +1030

    added degrees symbol

--- assets/script.js
+++ assets/script.js
@@ -66,7 +66,7 @@ function showWeatherData (data){
     currentWeatherItemsEl.innerHTML = 
     `<div class="weather-item">
         <div>Temperature</div>
-        <div>${temp}%</div>
+        <div>${temp}&#176; C</div>
     </div>
     <div class="weather-item">
         <div>Humidity</div>
@@ -101,8 +101,8 @@ function showWeatherData (data){
             <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
             <div class="other">
                 <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
-                <div class="temp">Night - ${day.temp.night}&#176;C</div>
-                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.day}&#176; C</div>
             </div>
             
             `
@@ -111,8 +111,8 @@ function showWeatherData (data){
             <div class="weather-forecast-item">
                 <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
                 <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <div class="temp">Night - ${day.temp.night}&#176;C</div>
-                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+                <div class="temp">Night - ${day.temp.night}&#176; C</div>
+                <div class="temp">Day - ${day.temp.day}&#176; C</div>
             </div>
             
             `
